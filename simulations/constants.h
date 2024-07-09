0df9715f3cd5c1cc660257862a9c42fbaba3bb25
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2024-07-09 02:30:18 -0500

    Create constants.h

---
+++ simulations/constants.h
@@ -0,0 +1,23 @@
+/*
+Default values (just in case)
+@author: Jeysson4K
+*/
+#define RADIUS 1.0f
+#define XCENTER 0.f
+#define YCENTER 0.f
+#define FRICTION 0.3f
+#define RESTITUTION 0.f
+#define DENSITY 1.f
+#define DEFAULT_POSX 0.f
+#define DEFAULT_POSY 25.0f
+#define FRAMES 256
+#define FPS 1.0f / 60.0f
+#define V_ITS 6
+#define POS_ITS 2
+#define XGRAVITY 0.f
+#define YGRAVITY 9.81f
+#define WORLD_WIDTH 50.f
+#define WORLD_HEIGHT 10.f
+#define WORLD_XPOS 0.f
+#define WORLD_YPOS 10.f
+#define WORLD_DENSITY 0.f
